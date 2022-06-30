8a1110c13a51d30fd5205e28c64e250bd8960b9f
Nahid30 <nahidshuvo30@gmail.com>
2022-06-30 23:26:26 +0600

    using delete method for delete a perticular data by using dalete method

--- src/Pages/HomePage/TableRow.js
+++ src/Pages/HomePage/TableRow.js
@@ -3,6 +3,24 @@ import React from 'react';
 const TableRow = ({ bill }) => {
     const { _id, name, email, phone, price } = bill;
 
+    // const handleDelete = id => {
+    //     console.log('mama deleted');
+    // }
+
+    const handleDelete = id => {
+        const proceed = window.confirm('Are you sure You want to Delete?')
+        if (proceed) {
+            const url = `http://localhost:5000/bill/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                })
+        }
+    }
+
     return (
 
         <tr>
@@ -15,7 +33,7 @@ const TableRow = ({ bill }) => {
                 {_id ? <button className='btn btn-link '>Edit</button> : <p>Failed</p>}
             </td> 
             <td>
-                {_id ? <button className='btn btn-link '>Delete</button> : <p>Failed</p>}
+                {_id ? <button onClick={ ()=>handleDelete(_id) } className='btn btn-link '>Delete</button> : <p>Failed</p>}
             </td>
             
         </tr >
